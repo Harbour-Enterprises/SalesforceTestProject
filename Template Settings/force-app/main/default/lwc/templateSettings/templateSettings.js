dfba70549534bb3ee4c45db840d2aad6ca7ba27e
mwariwada <mwariwada@gmail.com>
2024-09-27 02:21:10 +0200

    before obj mapping

--- Template Settings/force-app/main/default/lwc/templateSettings/templateSettings.js
+++ Template Settings/force-app/main/default/lwc/templateSettings/templateSettings.js
@@ -1,18 +1,67 @@
 import { LightningElement, track } from "lwc";
+import getTemplates from "@salesforce/apex/TemplateSettings.getTemplates";
 
 export default class TemplateSettings extends LightningElement {
-  @track availableTemplates = [
-    { label: "1", value: "1" },
-    { label: "2", value: "2" },
-    { label: "3", value: "3" }
-  ];
-  @track selectedTemplate;
+  @track availableTemplates = [];
+  @track selectedTemplate = "";
+  @track templateInputs = [];
+  @track objects = []; // Array to hold object names
+  @track fields = {}; // Object to hold fields by object name
 
-  //   async getTemplates(){
-  //     try()
-  //   }
+  fullTemplateData = [];
+
+  connectedCallback() {
+    this.loadTemplates();
+    this.getObjectsAndFields();
+  }
+
+  getObjectsAndFields() {
+    this.objects = ["Account", "Contact", "Opportunity"];
+    this.fields = {
+      Account: ["Name", "Type", "Industry"],
+      Contact: ["FirstName", "LastName", "Email"],
+      Opportunity: ["StageName", "Amount", "CloseDate"]
+    };
+  }
+
+  async loadTemplates() {
+    try {
+      const templates = await getTemplates();
+      this.fullTemplateData = templates;
+      this.availableTemplates = templates.map((template) => {
+        // The template value is used in both places because the type may not be unique but the title likely would be
+        return { label: template.value, value: template.value };
+      });
+    } catch (error) {
+      console.error("Error fetching templates:", error);
+    }
+  }
 
   handleSelectedTemplateChange(event) {
     this.selectedTemplate = event.detail.value;
+
+    // Find the selected template and set its inputs
+    const selectedTemplateData = this.fullTemplateData.find(
+      (template) => template.value === this.selectedTemplate
+    );
+    if (selectedTemplateData && selectedTemplateData.document_inputs) {
+      this.templateInputs = selectedTemplateData.document_inputs.map(
+        (input) => ({
+          id: input.id,
+          label: input.internal_label,
+          required: input.is_required,
+          placeholder: input.place_holder,
+          preferredIcon: input.preferred_icon,
+          isTextInput: input.field_type === "TEXTINPUT",
+          isImageInput: input.field_type === "IMAGEINPUT"
+        })
+      );
+    }
+  }
+
+  handleObjectChange(event, inputId) {
+    // Handle the object selection change
+    const selectedObject = event.detail.value;
+    // You can do something with the selected object here, if needed
   }
 }
